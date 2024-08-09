b21703b16b8997acd336adfcd0dcf4340cd88bdd
Patrick Lang <patrick@Patricks-Laptop.local>
2024-08-09 19:45:46 -0400

    remove vercel

--- client/src/contexts/socketProvider.jsx
+++ client/src/contexts/socketProvider.jsx
@@ -11,8 +11,7 @@ export function SocketProvider({ children }) {
     const [socket, setSocket] = useState();
 
     useEffect(() => {
-        // const newSocket = io('http://localhost:4000');
-        const newSocket = io('https://ttt-3d-new-server.vercel.app');
+        const newSocket = io('http://localhost:4000');
         setSocket(newSocket);
 
         return () => newSocket.close();
